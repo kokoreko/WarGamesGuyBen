c9171c51ac4d807a749d413e74a90935c53818c1
kosta <kokoreko@gmail.com>
2014-10-09 17:42:08 +0300

    Remote missiles/RemoteMissile.java is used for network communication

--- src/TzukEitan/missiles/EnemyMissile.java
+++ src/TzukEitan/missiles/EnemyMissile.java
@@ -1,5 +1,6 @@
 package TzukEitan.missiles;
 
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -8,7 +9,8 @@ import TzukEitan.utils.Utils;
 import TzukEitan.war.WarStatistics;
 
 /** Enemy missile, is been created by the Enemy launcher **/
-public class EnemyMissile extends Thread {
+public class EnemyMissile extends Thread{
+	
 	private List<WarEventListener> allListeners;
 
 	private String id;
@@ -20,6 +22,21 @@ public class EnemyMissile extends Thread {
 	private String launchTime;
 	private boolean beenHit = false;
 
+	/**
+	 * EnemyMissile constructor for remote client
+	 * @param id
+	 * @param destination
+	 * @param flyTime
+	 * @param damage
+	 */
+	public EnemyMissile(RemoteMissile m) {
+		allListeners = new LinkedList<WarEventListener>();
+		this.id = m.getId();
+		this.destination = m.getDestination();
+		this.flyTime = m.getFlyTime();
+		this.damage = m.getDamage();
+	}
+	
 	public EnemyMissile(String id, String destination, int flyTime, int damage,
 			String whoLaunchedMeId, WarStatistics statistics) {
 		allListeners = new LinkedList<WarEventListener>();
@@ -91,6 +108,13 @@ public class EnemyMissile extends Thread {
 
 	public int getDamage() {
 		return damage;
+	}
+	
+	public void setStatistics(WarStatistics s){
+		this.statistics = s;
+	}
+	public void setWhoLaunchedMeId(String id){
+		this.whoLaunchedMeId=id;
 	}
 	
 	public boolean isBeenHit(){
---
+++ src/TzukEitan/missiles/RemoteMissile.java
@@ -0,0 +1,59 @@
+package TzukEitan.missiles;
+
+import java.io.Serializable;
+
+
+/** Enemy missile, is been created by the Enemy launcher **/
+public class RemoteMissile implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -885540752735582183L;
+
+
+	private String id;
+	private String whoLaunchedMeId;
+	private String destination;
+	private int flyTime;
+	private int damage;
+	private String launchTime;
+	/**
+	 * EnemyMissile constructor for remote client
+	 * @param id
+	 * @param destination
+	 * @param flyTime
+	 * @param damage
+	 */
+	public RemoteMissile(String id, String destination, int flyTime, int damage) {
+		this.id = id;
+		this.destination = destination;
+		this.flyTime = flyTime;
+		this.damage = damage;
+	}
+	public String getId() {
+		return id;
+	}
+	public String getWhoLaunchedMeId() {
+		return whoLaunchedMeId;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public int getFlyTime() {
+		return flyTime;
+	}
+	public int getDamage() {
+		return damage;
+	}
+	public String getLaunchTime() {
+		return launchTime;
+	}
+	
+	
+
+	
+
+
+	
+
+}
